a38eb0f6b0073f592047c588d267efeae813c7f9
shrestharajat <shrestharajat@hotmail.com>
2021-04-13 11:50:43 +0545

    update app

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World! v1"
+def index():
+    return "Hello World! v2"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
